5552db188db7d05525ec6c6f51d11534e54a6deb
Mauricio Guerrero <maurizioguerrero@hotmail.com>
2023-05-15 17:31:26 -0500

    reducers

--- src/screens/ProductDetailsScreen.js
+++ src/screens/ProductDetailsScreen.js
@@ -1,9 +1,10 @@
 import { StyleSheet, View, Text, Image, FlatList, useWindowDimensions, ScrollView, 
 Pressable } from "react-native";
 import products from "../data/products";
+import { useSelector } from "react-redux";
 
 const ProductDetailsScreen = () => {
-  const product = products[0];
+  const product = useSelector(state => state.products.selectedProduct);
 
   const { width } = useWindowDimensions();
 
--- src/screens/productsScreen.js
+++ src/screens/productsScreen.js
@@ -1,36 +1,58 @@
-import { StyleSheet, Text, View, Image, FlatList, Pressable } from "react-native";
-import products from '../data/products'
+import { useNavigation } from "@react-navigation/native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  Pressable,
+} from "react-native";
+import {useSelector, useDispatch} from 'react-redux';
+import { productsSlice } from '../store/productsSlice';
 
 
 const ProductsScreen = ({ navigation }) => {
-    return (
-        <FlatList
-        data={products}
-        renderItem={({ item }) => (
-          <Pressable onPress={() => navigation.navigate('Product Details')} style={styles.itemContainer}>
-            <Image source={{uri: item.image}} style={styles.image}/>
-          </Pressable>
-        )}
-        numColumns={2}
-      />
-    );
+  // const navigation = useNavigation();
+
+  const dispatch = useDispatch();
+
+  const products = useSelector(state => state.products.products);
+
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <Pressable
+          onPress={() => {
+            // update selected product
+            dispatch(productsSlice.actions.setSelectedProduct(item.id))
+
+            navigation.navigate("Product Details")}}
+          style={styles.itemContainer}
+        >
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </Pressable>
+      )}
+      numColumns={2}
+    />
+  );
 };
-   
+
 const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          backgroundColor: "#fff",
-          alignItems: "center",
-          justifyContent: "center",
-        },
-        itemContainer: {
-          width: "50%",
-          padding: 1,
-        },
-        image: {
-          width: "100%",
-          aspectRatio: 1,
-        },
-    });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
 
 export default ProductsScreen;
